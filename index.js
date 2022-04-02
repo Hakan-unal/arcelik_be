490b030851eab4cfabc1569d5115d809ea316924
Con Sınov <hakanunal@devtagon.com>
2022-04-02 21:24:24 +0300

    update

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/build/index.html'));
 });
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public/build')))
 
 
 app.post("/test", (req, res) => {
