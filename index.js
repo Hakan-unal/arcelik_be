ad18922e89f2464b1de1759c956fbfea6d0047d0
Con Sınov <hakanunal@devtagon.com>
2022-04-02 14:50:10 +0300

    update

--- index.js
+++ index.js
@@ -19,20 +19,25 @@ app.use(cors({ origin: true }));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+
     next();
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "text/*" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ origin: true }));
 
 app.get("/test", (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.json({ message: "Hello world wORLD" });
+    res.json({ message: "Hello world" });
+})
+
+app.post("/test", (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.json({ message: "Post" });
 })
 
 app.post("/authenticate", (req, res) => {
@@ -80,7 +85,7 @@ app.post("/authenticate", (req, res) => {
         });
 });
 
-const port = process.env.PORT || 5050
+const port = 5050
 app.listen(port, () => {
     console.log("Listen port " + port)
 });
